b569f1be7a542bb365a4a265babe2d0865d29c6d
Charles Loehle <clguitar1@gmail.com>
2020-06-03 15:21:39 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+//const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+//connectDB();
+
+const app = express();
+
+// Body parser
+//app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
